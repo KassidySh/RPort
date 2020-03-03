f9cbca9ce953399e9d6a449eb1fe1ebf5504bfd5
KASSIDY_SHADE <kassie2129@gmail.com>
2020-03-02 20:36:22 -0600

    basic portfolio setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link, Route, Switch } from 'react-router-dom'
+
+import About from './About'
+import Project from './Project'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <main className='Main'>
+      <nav class="nav nav-pills nav-fill">
+        <li class="nav-item">
+          <Link to='/' className="nav-link"><h3>Home</h3></Link>
+</li>
+          <li className="nav-item">
+          <Link to='/project' class="nav-link"><h3>Projects</h3></Link>
+        </li>
+      </nav>
+      <div className='about'>
+        <Switch>
+          <Route exact path='/' component={About} />
+          <Route exact path='/project' component={Project} />
+        </Switch>
+      </div>
+    </main>
   );
 }
 
