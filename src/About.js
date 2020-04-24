ff7b982898299ebc11a256d3f483c5daa7a6df3c
KASSIDY_SHADE <kassie2129@gmail.com>
2020-04-23 20:33:17 -0500

    changed background color and updated the links

--- src/About.js
+++ src/About.js
@@ -5,25 +5,31 @@ import { Link } from 'react-router-dom'
 function About() {
     return (
         <div className='about'>
-            <div className='imgInfo'>
-                <img className='me' src="https://res.cloudinary.com/dt5zs08ue/image/upload/v1583201327/port/Screen_Shot_2020-03-02_at_8.08.31_PM_ghsfr1.png"></img>
-                <p><h3>I'm a software engineer from San Antonio and I have a passion for coding, superheros, books, and music. I recently finished a software engineering bootcamp with General Assembly and I enjoy working with React, Python and Django.</h3> </p>
+            <div className='aboutMain'>
+                <div className='imgInfo'>
+                    <img className='me' src="https://res.cloudinary.com/dt5zs08ue/image/upload/v1583201327/port/Screen_Shot_2020-03-02_at_8.08.31_PM_ghsfr1.png"></img>
+                    <p><h3 className='infoAboutMe' >I'm a software engineer from San Antonio and I have a passion for coding, superheros, books, and music. I recently finished a software engineering bootcamp with General Assembly and I enjoy working with React, Python, Django, and Drupal.</h3> </p>
+                </div>
+                <ul className='skills'>
+                    <li>HTML</li>
+                    <li>JAVASCRIPT</li>
+                    <li>CSS</li>
+                    <li>REACT</li>
+                    <li>MONGODB</li>
+                    <li>EXPRESS</li>
+                    <li>DJANGO</li>
+                    <li>PYTHON</li>
+                    <li>BOOTSTRAP</li>
+                    <li>DRUPAL</li>
+                </ul>
             </div>
-            <ul>
-                <li>HTML</li>
-                <li>JAVASCRIPT</li>
-                <li>CSS</li>
-                <li>REACT</li>
-                <li>MONGOOSE</li>
-                <li>EXPRESS</li>
-                <li>DJANGO</li>
-                <li>PYTHON</li>
-            </ul>
-            <ul>
-                <ul><a className='text-dark' target="_blank" href="https://www.linkedin.com/in/kassidyshade/"><h4>LinkedIn</h4></a></ul>
-                <ul><a className='text-dark' target="_blank" href="https://github.com/KassidySh"><h4>My Github</h4></a></ul>
-                <ul><a className='text-dark' href="mailto:kassidysha@gmail.com"><h4>Email Me</h4></a></ul>
-            </ul>
+            <div className='contact'>
+                <ul className='links'>
+                    <ul><a className='text-dark' target="_blank" href="https://www.linkedin.com/in/kassidyshade/"><h4>LinkedIn</h4></a></ul>
+                    <ul><a className='text-dark' target="_blank" href="https://github.com/KassidySh"><h4>My Github</h4></a></ul>
+                    <ul><a className='text-dark' href="mailto:kassidysha@gmail.com"><h4>Email Me</h4></a></ul>
+                </ul>
+            </div>
         </div>
     )
 }
--- src/Project.js
+++ src/Project.js
@@ -6,54 +6,58 @@ function Project() {
 
     return (
         <div className='project'>
-            <a target="_blank" href="https://kassidysh.github.io/project1-Trivia/">
-                <div className="card mb-3">
-                    <div className="row no-gutters">
-                        <div className="col-md-4">
-                            <img src="https://res.cloudinary.com/dt5zs08ue/image/upload/v1583200009/port/Books_hqqael.png" className="card-img" alt="All About Books" />
-                        </div>
-                        <div className="col-md-8">
-                            <div className="card-body">
+
+            <div className="card mb-3">
+                <div className="row no-gutters">
+                    <div className="col-md-4">
+                        <img src="https://res.cloudinary.com/dt5zs08ue/image/upload/v1583200009/port/Books_hqqael.png" className="card-img" alt="All About Books" />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <a target="_blank" href="https://kassidysh.github.io/project1-Trivia/">
                                 <h5 className="card-title">All About Books</h5>
-                                <p className="card-text">This is a quiz app made with an API call. The API calls 40 diffrent questions that are then shuffled to show 10 diffrent questions each time you play the game. There are four answers that you can choose from and if you answer in 5 seconds you get 10 points, if you don't you'll get 5 points.</p>
-                                <p className="card-text"><small className="text-muted">HTML, CSS, JAVASCRIPT</small></p>
-                            </div>
+                            </a>
+                            <p className="card-text">This is a quiz app made with an API call. The API calls 40 diffrent questions that are then shuffled to show 10 diffrent questions each time you play the game. There are four answers that you can choose from and if you answer in 5 seconds you get 10 points, if you don't you'll get 5 points.</p>
+                            <p className="card-text"><small className="text-muted">HTML, CSS, JAVASCRIPT</small></p>
                         </div>
                     </div>
                 </div>
-            </a>
-            <a target="_blank" href="https://superfacts.netlify.com/">
-                <div className="card mb-3">
-                    <div className="row no-gutters">
-                        <div className="col-md-4">
-                            <img src="https://res.cloudinary.com/dt5zs08ue/image/upload/v1583200021/port/Super_ldawsq.png" className="card-img" alt="Super Facts" />
-                        </div>
-                        <div className="col-md-8">
-                            <div className="card-body">
+            </div>
+
+
+            <div className="card mb-3">
+                <div className="row no-gutters">
+                    <div className="col-md-4">
+                        <img src="https://res.cloudinary.com/dt5zs08ue/image/upload/v1583200021/port/Super_ldawsq.png" className="card-img" alt="Super Facts" />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <a target="_blank" href="https://superfacts.netlify.com/">
                                 <h5 className="card-title">Super Facts</h5>
-                                <p className="card-text">This is a site to look through various superheros and see all sorts of information about them. It uses an API call to get the information. There's also a search bar that you can use to search people.</p>
-                                <p className="card-text"><small className="text-muted">REACT, CSS</small></p>
-                            </div>
+                            </a>
+                            <p className="card-text">This is a site to look through various superheros and see all sorts of information about them. It uses an API call to get the information. There's also a search bar that you can use to search people.</p>
+                            <p className="card-text"><small className="text-muted">REACT, CSS</small></p>
                         </div>
                     </div>
                 </div>
-            </a>
-            <a target="_blank" href="https://mythinformation.herokuapp.com/">
-                <div className="card mb-3">
-                    <div className="row no-gutters">
-                        <div className="col-md-4">
-                            <img src="https://res.cloudinary.com/dt5zs08ue/image/upload/v1583200016/port/Myth_j3lggy.png" className="card-img" alt="Myth Information" />
-                        </div>
-                        <div className="col-md-8">
-                            <div className="card-body">
+            </div>
+
+            <div className="card mb-3">
+                <div className="row no-gutters">
+                    <div className="col-md-4">
+                        <img src="https://res.cloudinary.com/dt5zs08ue/image/upload/v1583200016/port/Myth_j3lggy.png" className="card-img" alt="Myth Information" />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <a target="_blank" href="https://mythinformation.herokuapp.com/">
                                 <h5 className="card-title">Myth Information</h5>
-                                <p className="card-text">Myth Information uses a database to store information on diffrent gods and stories about the gods. You have to be logged in to add stories and only the author can edit or delete a story. A user can search by god and by what something is god of.</p>
-                                <p className="card-text"><small className="text-muted">DJANGO, PYTHON, BOOTSTRAP, PSQL</small></p>
-                            </div>
+                            </a>
+                            <p className="card-text">Myth Information uses a database to store information on diffrent gods and stories about the gods. You have to be logged in to add stories and only the author can edit or delete a story. A user can search by god and by what something is god of.</p>
+                            <p className="card-text"><small className="text-muted">DJANGO, PYTHON, BOOTSTRAP, PSQL</small></p>
                         </div>
                     </div>
                 </div>
-            </a>
+            </div>
         </div>
     )
 }
